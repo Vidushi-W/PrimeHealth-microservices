acbcf4eb068ed84bbbd4fd9018aec6068df241dd
Vidushi <it23281400@my.sliit.lk>
2026-04-16 18:45:22 +0530

    Add doctor profile route to App

--- Frontend_UI/src/App.jsx
+++ Frontend_UI/src/App.jsx
@@ -2,12 +2,14 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import DoctorListPage from './pages/DoctorListPage';
 import DoctorDetailsPage from './pages/DoctorDetailsPage';
+import DoctorProfilePage from './pages/DoctorProfilePage';
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<DoctorListPage />} />
+        <Route path="/profile" element={<DoctorProfilePage />} />
         <Route path="/doctors/:doctorId" element={<DoctorDetailsPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
